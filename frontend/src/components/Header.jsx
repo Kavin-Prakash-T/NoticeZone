f2ec727c30699f87f89cd3208f23d45440775599
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-12-26 22:50:37 +0530

    feat: Implement conditional endering for navbar based on admin/user login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,23 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router"
 
 const Header = () => {
+
+  const [role, setRole] = useState(sessionStorage.getItem("role"))
+  useEffect(() => {
+    setRole(sessionStorage.getItem("role"))
+  }, [])
+
   return (
     <nav className="flex justify-between border-b border-[#73E6CB] shadow-sm text-[#0A3C30] items-center p-4 bg-[#ECFDF7]">
       <Link to="/" className="flex gap-2 items-center">
-      <img src="/logo.png" alt="logo" className="rounded-[50px] w-12.5 h-12.5"/>
-      <h1 className="text-2xl font-bold">NoticeZone</h1>
+        <img src="/logo.png" alt="logo" className="rounded-[50px] w-12.5 h-12.5" />
+        <h1 className="text-2xl font-bold">NoticeZone</h1>
       </Link>
       <div className="flex gap-8 items-center ">
         <Link to="/">Home</Link>
         <Link to="/notices">All Notices</Link>
-        <Link to="/admin">Admin</Link>
+        {role === "admin" && <Link to="/admin">Admin</Link>}
         <Link to="/login"> <button className="bg-[#00674F] text-white text px-3 pb-2 pt-1 rounded-lg hover:bg-[#0A3C30]">
           Login
         </button></Link>
