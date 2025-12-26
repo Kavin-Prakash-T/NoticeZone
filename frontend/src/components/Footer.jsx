50f80c7755cb3f92811d8de9de63cba9c9caa1bf
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-12-26 22:01:36 +0530

    style: Update styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-center p-4 bg-[#ECFDF7] text-[#0A3C30]">
+    <footer className="text-center p-4 bg-[#0A3C30] text-[#ECFDF7]">
       <p>© 2025 Designed and developed by Kavin. All rights reserved.</p>
     </footer>
   )
