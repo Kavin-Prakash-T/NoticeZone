4a3176d0306659e573811258ff7e0f723c941a9f
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-12-26 21:29:08 +0530

    feat: Implement footer section

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-center p-4 bg-[#ECFDF7] text-[#0A3C30]">
+      <p>© 2025 Designed and developed by Kavin. All rights reserved.</p>
+    </footer>
   )
 }
 
